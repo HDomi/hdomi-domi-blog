51523d4ef59d4dec8bf1a431d0cbc86ef524da3b
HJY <hwangjae1139@gmail.com>
2022-12-27 11:18:21 +0900

    test commmit

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hdomi.github.io/",
+  base: "",
   plugins: [
     ghPages(
       {
