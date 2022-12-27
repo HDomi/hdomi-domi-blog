f5a24d7b032f7b411a2f5ec82f7365d08940ece2
HJY <hwangjae1139@gmail.com>
2022-12-27 11:10:39 +0900

    update gh-pages setting

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hdomi-domi-blog/",
+  base: "/hdomi.github.io/",
   plugins: [
     ghPages(
       {
