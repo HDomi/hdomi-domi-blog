2f7c59c28aa072ef572611c4f787b8277bae3cbd
HJY <hwangjae1139@gmail.com>
2022-12-27 11:41:44 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [
     vue({
       include: [/\.vue$/],
