f06b5d504e1436a329cb89c9ff010af20074ecaf
HJY <hwangjae1139@gmail.com>
2022-12-29 14:19:23 +0900

    'hosting'

---
+++ dist/assets/AboutPage.890ff4e8.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as s,a as o}from"./index.9da84784.js";const a={components:{},mixins:[],props:{},data(){return{}},computed:{},presets:{},watch:{},mounted(){},methods:{}},c={class:"page-wrap scrollBar"},n=o("div",{class:"main-title"},"About",-1),r=[n];function p(_,d,i,m,l,u){return s(),e("div",c,r)}const h=t(a,[["render",p]]);export{h as default};
