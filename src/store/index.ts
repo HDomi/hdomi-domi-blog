58fecbe0928a7bc413e4aa39f20dc3454f08a5b3
HJY <hwangjae1139@gmail.com>
2023-01-16 16:12:30 +0900

    fix: error fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,10 @@ import Vuex from 'vuex'
 
 export default new Vuex.Store({
   state: {
-    isNavOpen: false
   },
   getters: {
   },
   mutations: {
-    toggleNav() {
-      this.state.isNavOpen = !this.state.isNavOpen
-    }
   },
   actions: {
   },
